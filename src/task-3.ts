fa2587def80c4db0621104e07d4f217e6fa78341
HOG <hogseruj1@gmail.com>
2025-10-23 23:04:28 +0300

    Added TS tasks.

---
+++ src/task-3.ts
@@ -0,0 +1,21 @@
+// Масив рядків
+const usernames: string[] = ["alice", "bob", "charlie"];
+
+// Масив чисел
+const ratings: number[] = [4.5, 3.8, 5];
+
+// Інтерфейс для елементів масиву products
+interface Product {
+  id: number;
+  title: string;
+}
+
+// Масив об’єктів з типом Product
+const products: Product[] = [
+  { id: 1, title: "Phone" },
+  { id: 2, title: "Laptop" },
+];
+
+console.log(`Usernames: ${JSON.stringify(usernames)}`);
+console.log(`Ratings: ${JSON.stringify(ratings)}`);
+console.log(`Products: ${JSON.stringify(products)}`);
